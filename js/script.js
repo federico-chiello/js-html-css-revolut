b24a911e31765e1a852d9d86dfe19e549f7780a6
Fede <chiellofederico@gmail.com>
2021-01-12 17:24:43 +0100

    menu

--- js/script.js
+++ js/script.js
@@ -3,12 +3,8 @@ $(document).ready(function(){
   $('.dropdown').hover(function(){
     // $('.dropdown-menu').toggleClass('active');
     $(this).children('.dropdown-menu').toggleClass('active');
-    // $('.dropdown > a').css('color', 'black');
 
-
+    
 
   });
-
-
-
 });
