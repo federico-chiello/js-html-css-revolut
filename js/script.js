80bb815ef3a8a5eb8d86713515dbbb1b3a63e7df
Fede <chiellofederico@gmail.com>
2021-01-12 19:32:33 +0100

    modifiche al dropdown-menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 
+  // Quando si passa con il mouse sopra i menu la scritta si colora di nero
   $('.dropdown').hover(function(){
     $(this).children('.dropdown-menu').toggleClass('active');
     $(this).children('a').css('color', 'black');
   });
-
+  // Quando si toglie il mouse sopra i menu la scritta si colora di grigio
   $(".dropdown").mouseleave(function() {
     $(this).children('a').css('color', 'grey');
   });
