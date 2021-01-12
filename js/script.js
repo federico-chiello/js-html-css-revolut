f20824277509c099024b41c289a8eddf60fb6cca
Fede <chiellofederico@gmail.com>
2021-01-12 16:51:15 +0100

    aggiunta altri menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 
   $('.dropdown').hover(function(){
-    $('.dropdown-menu').toggleClass('active');
+    // $('.dropdown-menu').toggleClass('active');
+    $(this).children('.dropdown-menu').toggleClass('active');
+    // $('.dropdown > a').css('color', 'black');
+
+
 
   });
 
